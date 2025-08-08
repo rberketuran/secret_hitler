e0b2477894c4baa6ec2dc7ea8952d80c0cb7b1c3
rberketuran <z3r0.dusty@gmail.com>
2025-08-08 17:44:45 +0300

    template server setup with socket

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,34 @@
+// ciziktir.io
 import express from 'express';
+import { Server } from 'socket.io';
+import http from 'http';
 import dotenv from 'dotenv';
 
+const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: '*', // allow all for dev
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on('joinRoom', (roomCode) => {
+        socket.join(roomCode);
+        console.log(`${socket.id} joined room ${roomCode}`);
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`User disconnected: ${socket.id}`);
+    });
+});
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
